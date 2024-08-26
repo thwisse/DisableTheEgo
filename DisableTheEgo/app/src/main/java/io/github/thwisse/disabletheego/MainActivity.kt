90d009b9df2e36998c929c77bf9d959749255315
thwisse <thwisse@tutanota.com>
2024-08-26 21:42:05 +0300

    host fragment eklendi, fragmentlar yaratildi, graph yaratildi

--- DisableTheEgo/app/src/main/java/io/github/thwisse/disabletheego/MainActivity.kt
+++ DisableTheEgo/app/src/main/java/io/github/thwisse/disabletheego/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import io.github.thwisse.disabletheego.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        setContentView(view)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navController = getNavController()
+    }
+
+    fun getNavController(): NavController {
+        val navHostFragmentView =
+            supportFragmentManager.findFragmentById(R.id.navHostFragmentView) as NavHostFragment
+
+        return navHostFragmentView.navController
     }
 }
